638af0eb3d6e239dd0bbbb2f044a24167f8d1b8c
shine-HFrao <raohuifei@163.com>
2022-09-15 14:16:14 +0800

    update chunk

--- shine_hfrao-lodash.js
+++ shine_hfrao-lodash.js
@@ -1,5 +1,25 @@
 var shine_hfrao = {
-  chunk: function () {
+  chunk: function (array, size = 1) {
+    var resultAry = []
+    var itemAry = []
+    var count = 0
 
-  },
+    for (var i = 0; i < array.length; i++) {
+      count++
+      if (count <= size) {
+        itemAry.push(array[i])
+      } else if (count > size) {
+        count = 0
+        resultAry.push(itemAry.slice())
+        itemAry = []
+        itemAry.push(array[i])
+      }
+    }
+
+    if (itemAry.length) {
+      resultAry.push(itemAry.slice())
+    }
+
+    return resultAry
+  }
 }
